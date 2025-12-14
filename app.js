1d09005b09cfd1c89fd1b8336021e64bfaa64f7a
Abiramasundari10 <abiyadav10496@gmail.com>
2025-12-14 23:38:43 +0530

    new update

--- app.js
+++ app.js
@@ -17,19 +17,19 @@ const parent = React.createElement(
     { id: "parent" }, [React.createElement(
         "div",
         { id: "child" },[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")
+        React.createElement("h1", { key: "h1-1"}, "I'm an h1 tag"),
+        React.createElement("h2", { key: "h2-1"}, "I'm an h2 tag")
 
         ]),
     React.createElement(
         "div",
         { id: "child1" },[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag")]
+        React.createElement("h1", { key: "h1-2"}, "I'm an h1 tag"),
+        React.createElement("h2", { key: "h2-2"}, "I'm an h2 tag")]
     )]
 
 );
-console.log(parent);//object
+
 
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World");
