20f8db70122f3372fb197460eeb68bcd511f5875
Abiramasundari10 <abiyadav10496@gmail.com>
2025-12-14 21:34:36 +0530

    second commit

---
+++ dist/REACT.7c0ccee6.js
@@ -0,0 +1,32 @@
+/**
+ * <div id=parent>
+ *  <div id=child>
+ *  <h1>i'm the h1 tag</h1>
+ *  <h2>i'm the h2 tag</h2>
+ *  </div>
+ * 
+ * </div>
+ * ReactElement(object)=>HTML(Browser Understand)
+ */ //React core structure
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ])
+]);
+console.log(parent); //object
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=REACT.7c0ccee6.js.map
